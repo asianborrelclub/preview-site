6ca8bc496003bada4e49e1cef7043484df7b58e8
Ryan <ryan.j.z@hotmail.com>
2020-09-03 02:10:53 +0200

    Big
    - Sass toegevoegd
    - social logo toegevoegd
    - kleuren gefixed
    - inhoud toegevoegd
    
    TODO:
    - fix alignments
    - fix mobile view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import instagram from "./img/instagram.svg";
+import facebook from "./img/facebook.svg";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div className="soon">
+          <i>
+            Coming soon
+            <span>.</span>
+            <span>.</span>
+            <span>.</span>
+          </i>
+        </div>
+        <div className="abc">Asian Borrel Club</div>
+        <div className="socials">
+          Follow us for updates:
+          <a
+            className="insta"
+            href="https://www.instagram.com/asianborrelclub/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={instagram} alt="instagram.com" />
+            @asianborrelclub
+          </a>
+          <a
+            className="fb"
+            href="https://www.facebook.com/asianborrelclub/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={facebook} alt="facebook.com" />
+            /asianborrelclub
+          </a>
+        </div>
       </header>
     </div>
   );
