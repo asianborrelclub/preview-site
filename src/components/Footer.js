042f2f804dc1bc0206272e4ab95493658e171c76
Ryan <ryan.j.z@hotmail.com>
2020-11-18 04:47:27 +0100

    Added social buttons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Footer.scss";
 import logo from "../img/dark-h-logo-abc-full.svg";
+import SocialButton from "./SocialButton";
 
 class Footer extends Component {
   render() {
@@ -12,6 +13,7 @@ class Footer extends Component {
             Made&nbsp;with&nbsp;❤️ -&nbsp; ©&nbsp;Asian&nbsp;Borrel&nbsp;Club.
             All&nbsp;rights&nbsp;reserved.
           </span>
+          <SocialButton />
         </div>
       </footer>
     );
