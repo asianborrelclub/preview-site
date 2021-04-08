5ca6fd5cbc4e78a35b2e132f2db870df5fdf108b
Ryan <ryan.j.z@hotmail.com>
2021-04-08 04:42:07 +0200

    Added new routes
    Added committee application form route and committee route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.scss";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import Form from "./Pages/Form";
 import Home from "./Pages/Home";
+import Committee from "./Pages/Committee";
 
 function App() {
   return (
     <Router>
       <Route path="/" exact component={Home} />
-      <Route path="/form" exact component={Form} />
+      <Route path="/committee-form" exact component={Form} />
+      <Route path="/committees" exact component={Committee} />
     </Router>
   );
 }
