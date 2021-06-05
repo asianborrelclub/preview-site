bc6d9aba00b914f4e00f043ec13ca4d1dae766e5
Ryan <ryan.j.z@hotmail.com>
2021-06-05 19:54:32 +0200

    Add hoodie to navbar
    Add hoodie to navbar

--- src/components/NavbarItems.js
+++ src/components/NavbarItems.js
@@ -1,5 +1,10 @@
 export const NavbarItems = [
   {
+    title: "Hoodie",
+    url: "/hoodie",
+    cName: "nav-links",
+  },
+  {
     title: "Calendar",
     url: "https://www.asianborrelclub.nl/#calendar",
     cName: "nav-links",
@@ -21,8 +26,7 @@ export const NavbarItems = [
   },
   {
     title: "Register",
-    url:
-      "https://docs.google.com/forms/d/1y0BpGWZA2d8IKTOqrYGGHWD_9QzXTXVZRL3siw4v_KA/",
+    url: "https://docs.google.com/forms/d/1y0BpGWZA2d8IKTOqrYGGHWD_9QzXTXVZRL3siw4v_KA/",
     cName: "nav-links-mobile",
     target: "_blank",
     rel: "noopener noreferrer",
