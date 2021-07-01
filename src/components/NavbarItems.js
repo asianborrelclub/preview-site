f0e7b3cc48c635420970bbed95d2d63a4f64d051
Ryan <ryan.j.z@hotmail.com>
2021-07-02 01:12:25 +0200

    updated navbar
    updated navbar

--- src/components/NavbarItems.js
+++ src/components/NavbarItems.js
@@ -1,27 +1,29 @@
 export const NavbarItems = [
   {
-    title: "Hoodie",
-    url: "/hoodie",
+    title: "About",
+    url: "/#about-us",
     cName: "nav-links",
   },
   {
-    title: "Calendar",
-    url: "https://www.asianborrelclub.nl/#calendar",
-    cName: "nav-links",
-  },
-  {
-    title: "About ABC",
-    url: "https://www.asianborrelclub.nl/#about-us",
-    cName: "nav-links",
-  },
-  {
-    title: "Our Board",
-    url: "https://www.asianborrelclub.nl/#contact-page",
+    title: "Events",
+    url: "/#calendar",
     cName: "nav-links",
   },
   {
     title: "Committees",
     url: "/committees",
+    cName: "nav-links",
+  },
+
+  {
+    title: "Hoodie",
+    url: "/hoodie",
+    cName: "nav-links",
+  },
+
+  {
+    title: "Contact",
+    url: "/contact",
     cName: "nav-links",
   },
 ];
