0fede3f9c1aa11d9b551d6aa2822c3008dcf6796
Ryan <ryan.j.z@hotmail.com>
2021-07-17 01:48:20 +0200

    Partners section component
    - added Partners section
    - added partners card

---
+++ src/components/Partners.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import PartnerCard from "./PartnerCard";
+import "./Partners.scss";
+import fmgBanner from "../img/partners/fullmoon-garden/fmg_1.jpg";
+import fmgLogo from "../img/partners/fullmoon-garden/fmg_logo.png";
+import tgBanner from "../img/partners/tea-guys/tg_1.jpeg";
+import tgLogo from "../img/partners/tea-guys/tg_logo.png";
+import wgmmBanner from "../img/partners/wagamama/wgmm_1.jpeg";
+import wgmmLogo from "../img/partners/wagamama/wgmm_logo.png";
+import tfmBanner from "../img/partners/the-foodmarket/tfm_1.png";
+import tfmLogo from "../img/partners/the-foodmarket/tfm_logo.svg";
+
+class Partners extends Component {
+  render() {
+    return (
+      <div id="partners" className="partners">
+        <h1>Partners</h1>
+        <div className="partner-cards">
+          <PartnerCard
+            link="/partners/fullmoon-garden"
+            banner={fmgBanner}
+            logo={fmgLogo}
+          />
+          <PartnerCard
+            link="/partners/tea-guys"
+            banner={tgBanner}
+            logo={tgLogo}
+          />
+          <PartnerCard
+            link="/partners/fullmoon-garden"
+            banner={wgmmBanner}
+            logo={wgmmLogo}
+          />
+          <PartnerCard
+            link="/partners/fullmoon-garden"
+            banner={tfmBanner}
+            logo={tfmLogo}
+          />
+          <div className="partner-card-contact">...and more to come!</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Partners;
