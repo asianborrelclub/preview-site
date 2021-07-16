00efacfcb823db60c9e0eff673e81a1fab71947e
Ryan <ryan.j.z@hotmail.com>
2021-07-16 03:41:49 +0200

    Added template for Partners Page component
    Added template for Partners Page component

---
+++ src/components/PartnersContent.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import "./PartnersContent.scss";
+
+class PartnersContent extends Component {
+  render() {
+    return (
+      <div>
+        <div className="partners-template">
+          <div className="partners-banner">
+            <div className="partners-image">
+              <img
+                src={this.props.logo}
+                alt="partner"
+                className="partners-logo"
+              />
+            </div>
+          </div>
+          <div className="partners-info">
+            <div className="partners-title">
+              <h1>{this.props.title}</h1>
+            </div>
+            <div className="partners-about">
+              <div>{this.props.about}</div>
+              <div>{this.props.about2}</div>
+            </div>
+            <div className="partners-deal">
+              <h2>ABC Deal:</h2>
+              {this.props.deal}
+            </div>
+            <div className="partners-contact">
+              <h1>Contact</h1>
+              <div>
+                <i className="fas fa-utensils" />
+                <a href={this.props.website}>{this.props.website}</a>
+              </div>
+              <div>
+                <i className="fas fa-map-marker-alt"></i>
+                <a href={this.props.loc_link}>{this.props.loc}</a>
+              </div>
+              <div>
+                <i className="fas fa-phone-alt"></i>
+                <a href={"tel:" + this.props.phone2}>+31 20 2382546</a>
+              </div>
+            </div>
+
+            <div className="partners-discountpolicy">
+              Please click here to read our discount policy.
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PartnersContent;
