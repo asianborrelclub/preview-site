772c7168344f9127cd7b1ab70d0f2229ae1e1ef3
Ryan <ryan.j.z@hotmail.com>
2021-08-14 07:55:30 +0200

    Update gallery
    JPG broke netlify build -> changed to jpg ...

--- src/components/HomepageGallery.js
+++ src/components/HomepageGallery.js
@@ -8,7 +8,7 @@ import gallery3 from "../img/gallery/3.jpg";
 import gallery4 from "../img/gallery/4.jpg";
 import gallery5 from "../img/gallery/5.jpg";
 import gallery6 from "../img/gallery/6.jpeg";
-import gallery7 from "../img/gallery/7.JPG";
+import gallery7 from "../img/gallery/7.jpg";
 import gallery8 from "../img/gallery/8.jpeg";
 import gallery9 from "../img/gallery/9.jpeg";
 
