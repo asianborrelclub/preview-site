70f208503c8134be279e11254f751224f9f0004d
Ryan <ryan.j.z@hotmail.com>
2021-08-14 07:15:10 +0200

    Update Calendar
    Update Calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -14,11 +14,11 @@ class Calendar extends Component {
         ></img>
         <h1>Calendar</h1>
         <div className="event">
-          <CalendarItem date="04-07-2021" title="Extreme Sunday 2" />
-          <CalendarItem date="10-07-2021" title="Hoodie Meetup" />
-          <CalendarItem date="22-07-2021" title="Gartic Phone" />
-          <CalendarItem date="01-08-2021" title="Extreme Sunday 3" />
           <CalendarItem date="14-08-2021" title="Speeddating" />
+          <CalendarItem date="23-08-2021" title="ABC Beach Day" />
+          <CalendarItem date="11-09-2021" title="AMS Foodcrawl" />
+          <CalendarItem date="23-09-2021" title="Mid-Autumn Festival" />
+          <CalendarItem date="21-09-2021" title="Coming soon..." />
         </div>
         <div>Follow our socials to get updates!</div>
       </div>
