212166a58636701ef8baebfde9723873c4837cf4
Ryan <ryan.j.z@hotmail.com>
2021-08-14 05:31:25 +0200

    Update Committee info page
    Added IT Committee video
    Updated Marketing Committee video
    Updated npm packages

--- src/components/CommitteeContent.js
+++ src/components/CommitteeContent.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 
 import CommitteeItems from "./CommitteeItems";
 import "./CommitteeContent.scss";
-import IT from "../img/undraw_IT.svg";
 
 class CommitteeContent extends Component {
   render() {
@@ -34,7 +33,7 @@ class CommitteeContent extends Component {
             text="The Committee of Events has the responsibility to organize all events. Our goal is to create opportunities for students to hang out, meet new people and build friendships in a fun way. For now, the Asian Borrel Club has online events every two weeks, but in the near future we hope to hosts events in real life."
           />
           <CommitteeItems
-            url="https://www.youtube.com/embed/SzdnFdEkqgQ"
+            url="https://www.youtube.com/embed/0ooBKbDTYzo"
             name="Marketing"
             text="The Committee of Marketing has the responsibility to promote the Asian Borrel Club. We show our creativity by making our own designs for posters and stories on Instagram and Facebook, write texts for posts, take part of the video crew, create scripts for videos or design and set up our merchandise shop. We are also responsible for the communication through Social Media."
           />
@@ -43,21 +42,17 @@ class CommitteeContent extends Component {
             name="External Affairs"
             text="The Committee of External Affairs has the responsibility to contact third parties. Our main focus is expanding the network of Asian Borrel Club, concluding partnership deals with business partners with an affinity for the Asian culture and managing these contacts. Furthermore, we also facilitate and organise the interviews for the committee applications."
           />
-          <div className="committee-item">
-            <img src={IT} alt="Committee" className="committee-image" />
-            <div className="committee-context">
-              <h1 className="committee-name">IT</h1>
-              <p className="committee-text">
-                The Committee of IT has the responsibility to support all
-                technical aspects of the Asian Borrel Club. For example:
-                maintaining our website, moderating the technical platforms of
-                online events and structuring our Discord server. When we are
-                doing in-person events, we will be responsible for the technical
-                sides; setting up music, speakers, microphones, lights, or any
-                special effects.
-              </p>
-            </div>
-          </div>
+          <CommitteeItems
+            url="https://www.youtube.com/embed/LORF3NyLCeo"
+            name="IT"
+            text="The Committee of IT has the responsibility to support all
+            technical aspects of the Asian Borrel Club. For example:
+            maintaining our website, moderating the technical platforms of
+            online events and structuring our Discord server. When we are
+            doing in-person events, we will be responsible for the technical
+            sides; setting up music, speakers, microphones, lights, or any
+            special effects."
+          />
         </div>
         <div className="join-cta">
           <span>Interested in joining one of our committees?</span>
