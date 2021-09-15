3399de2e15fccf692bfc8f766418d221583d722b
Ryan <ryan.j.z@hotmail.com>
2021-09-15 21:34:18 +0200

    banner fix
    banner fix

--- src/Pages/PartnerYuansHotPot.js
+++ src/Pages/PartnerYuansHotPot.js
@@ -19,9 +19,11 @@ class PartnerYuansHotPot extends Component {
           logo={yhpLogo}
           title="Yuan's Hot Pot"
           about="What started back in the days as a simple but popular street food soon became one of the most popular dishes of the east.
-          For over 20 years Yuan’s have remained the leader in the industry and opened their first European restaurant in Amsterdam back in June 2018 🎋🐼🇳🇱"
+          For over 20 years Yuan’s have remained the leader in the industry and opened their first European restaurant in Amsterdam back in June 2018 🎋🐼🇳🇱
+          "
           about2="Come and try a little bit of Chengdu and a little bit of Amsterdam in one pot 🍲"
-          deal="Get 2 vouchers worth €10,00 each. These can be collected at one of our physical events."
+          deal="Get 2 vouchers worth €10,00 each. These can be collected at one of our physical events. (Vouchers valid until 31-08-2022, 
+            max. one voucher per table.)"
           website="https://yuanhotpot.com/"
           loc_link="https://www.google.com/maps/search/Yuan's+Hot+Pot/@52.3530363,4.8982841,14.08z"
           loc="Ferdinand Bolstraat 18, 1072 LJ Amsterdam"
