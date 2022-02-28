f31ac9a443005725b0d55651b6553456b6cef303
Ryan <ryan.j.z@hotmail.com>
2022-02-28 03:43:07 +0100

    update banner
    update banner

--- src/components/BannerPSA.js
+++ src/components/BannerPSA.js
@@ -1,26 +1,24 @@
 import React, { Component } from "react";
 import "./BannerPSA.scss";
 
-class BannerPSA extends Component {
+export default class BannerPSA extends Component {
   render() {
     return (
       <>
         <a className="banner-anchor" href="/board">
           <div className="scrolling-banner-PSA dt">
-            <div>Board recruitement for 2022/2023 is now in progress!</div>
+            <div>Board recruitment for 2022/2023 is now in progress!</div>
             <div>Click here for more info.</div>
           </div>
           <div className="scrolling-banner-PSA tablet">
-            <div>Board recruitement for year 22/23 now in progress!</div>
+            <div>Board recruitment for year 22/23 now in progress!</div>
             <div>Tap here for more info.</div>
           </div>
           <div className="scrolling-banner-PSA mobile">
-            22/23 Board recruitement now in progress. Tap here for more info.
+            22/23 Board recruitment now in progress. Tap here for more info.
           </div>
         </a>
       </>
     );
   }
 }
-
-export default BannerPSA;
